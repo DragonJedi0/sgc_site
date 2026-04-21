e703b8b0d9b7fecf1011b5134ebe42e0c416b950
Samuel Ybarra <sybarra2076@gmail.com>
2026-04-21 13:31:20 -0500

    add path.ts

---
+++ src/lib/paths.ts
@@ -0,0 +1,4 @@
+export const PERSONNEL_LIST = "/";
+export const PERSONNEL_DETAIL = "/personnel/:id"
+export const PERSONNEL_DETAIL_EDIT = "/personnel/:id/edit";
+export const PERSONNEL_FORM = "/personnel/new";
