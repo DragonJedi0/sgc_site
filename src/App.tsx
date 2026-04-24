21f2ac27f36d79cc5143de74ea718652eef36ce2
Samuel Ybarra <sybarra2076@gmail.com>
2026-04-24 13:08:44 -0500

    refactor: changing hardcoded paths to variables for easier editing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PersonnelList from './pages/PersonnelList';
 import PersonnelDetail from './pages/PersonnelDetail';
 import PersonnelForm from './pages/PersonnelForm';
+import { PATHS, ROUTES } from './lib/paths';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PersonnelList />} />
-        <Route path="/personnel/:id" element={<PersonnelDetail />} />
-        <Route path="/personnel/new" element={<PersonnelForm />} />
-        <Route path="/personnel/:id/edit" element={<PersonnelForm />} />
+        <Route path={PATHS.PERSONNEL_LIST} element={<PersonnelList />} />
+        <Route path={ROUTES.PERSONNEL_DETAIL} element={<PersonnelDetail />} />
+        <Route path={PATHS.PERSONNEL_NEW} element={<PersonnelForm />} />
+        <Route path={ROUTES.PERSONNEL_EDIT} element={<PersonnelForm />} />
       </Routes>
     </BrowserRouter>
   );
